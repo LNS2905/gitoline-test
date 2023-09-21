415ff4972934a51d0bbf51334ad68afc6211e382
NhatSangLe <nhatsang2905@gmail.com>
2023-09-22 00:12:26 +0700

    functional components

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,15 +1,19 @@
 import React from 'react'
-export default function Players() {
+import { Players } from '../shared/ListOfPlayers'
+export default function Player () {
+  
     return (
     <div className='container'>
+        {Players.map((player)=>(
            <div className='column'>
            <div className='card'>
-           <img src=''/>
-             <h3></h3>
-             <p className='title'></p>
+           <img src={player.img}/>
+             <h3>{player.name}</h3>
+             <p className='title'>{player.club}</p>
              <p><button>Detail</button></p>
            </div>
-         </div>    
+         </div>
+        ))}
     </div>
   )
-}
+        }
