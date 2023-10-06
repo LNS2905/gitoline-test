239fa9e1ee3b14d5d30fb2cbf1081bcf4db54267
NhatSangLe <nhatsang2905@gmail.com>
2023-10-06 08:57:06 +0700

    lab01-04

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import './App.css';
 import Films from './components/Films';
 import Footer from './components/Footer';
-
 import Navigation from './components/Navigation';
+import Detail from './components/Detail';
+import Contact from './components/Contact';
+
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
 function App() {
   return (
-    <div className='App'>
-      <Navigation/>
-      <Films/>
-      <Footer/>
+    <div>
+      <Navigation />
+      // <Routes>
+      //   <Route path='/' element={<Films />}>
+      //   </Route>
+      //   <Route path='/detail/:id' element={<Detail />}></Route>
+      //   <Route path='/contact' element={<Contact />}></Route>
+      // </Routes>
+      <Footer />
     </div>
-  );
+  )
 }
+
 export default App;
